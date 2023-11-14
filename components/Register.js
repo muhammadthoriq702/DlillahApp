4a3de57dfd0e80061ecc9146a2fb1b574691ea5e
kiranasyafa12 <kirana28.ks@gmail.com>
2023-11-14 13:30:10 +0700

    Login

---
+++ components/Register.js
@@ -0,0 +1,3 @@
+import {FlatList, StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import {MenuList, colors} from '../Constant';
---
+++ screens/Login.js
@@ -0,0 +1,13 @@
+import { Button, Text, View } from 'react-native'
+import React from 'react'
+import Login from '../components/Login'
+
+function KeranjangScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Login Akun</Text>
+      </View>
+    );
+  }
+
+export default KeranjangScreen
